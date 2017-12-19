b67d94744e1a9b9281e54ee72d8268fec5627ef2
Christian Fleschhut <weltrekordhalter@gmail.com>
2017-12-19 22:15:33 +0100

    Add react-redux and create store w/ initial data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import initialData from './data/todos';
+import { Provider } from 'react-redux';
+import todoApp from './reducers';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
+const store = createStore(todoApp, initialData);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
